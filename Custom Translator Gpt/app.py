474d41f74edc1ee8f62df672d1dcd11274d126f5
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2025-01-30 23:25:15 +0500

    optimized the code

--- Custom Translator Gpt/app.py
+++ Custom Translator Gpt/app.py
@@ -8,17 +8,7 @@ load_dotenv()
 
 llm = ChatGroq(model=os.getenv("MODEL"), api_key=os.getenv("DEEPSEEK_API_KEY"))\
 
-# user_input = st.text_input("ASK ME ANYTHING")
-# prompt = f"""You are an advanced AI translation model. Your task is to translate the given user input into 100 different languages and present the output strictly in table format.
 
-# Instructions:
-# Output only a structured table—no explanations, introductions, or additional text.
-# The table must have two columns:
-# Language (Full name of the language)
-# Translation (Accurate, natural translation of the input)
-# Do not include any preamble, summaries, or extra formatting outside the table.
-# User Input:
-# {user_input}"""
 
 st.title("🌐 Multilingual Translator")
 st.write("Translate your ✍️ text or  📄 PDF content into multiple languages with ease!")
@@ -117,10 +107,3 @@ if st.button("Translate Text"):
                 st.error("Please enter some text for translation.")
 
 
-
-# if user_input:
-#     response = model.invoke(prompt)
-#     st.write(response.content)
-
-# response = model.invoke("Hello , who are you?")
-# print(response.contents)
